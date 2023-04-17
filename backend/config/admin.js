5f24db161349697b1ca3d513a1d927208f29dd78
alpeza <80060968+alpeza@users.noreply.github.com>
2023-04-17 23:28:01 +0200

    up

--- backend/config/admin.js
+++ backend/config/admin.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'b64cc0a9a08b453ff483832956ddaa17'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'b64cc0a9a08b453ff483832956ddaa18'),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT', 'b64cc0a9a08b453ff483832956ddaa19'),
     },
   },
 });
